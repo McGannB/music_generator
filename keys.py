cf752dc4cae9bfc15c7a4a732021aa61cccc6e8c
Brendan McGann <bpmcgann@gmail.com>
2024-12-18 00:19:24 -0500

    Optimize get_keys Function

--- keys.py
+++ keys.py
@@ -1,70 +1,50 @@
 def get_key(root, key_type):
     key = [root]
     i = 1
-    match key_type:
-        case 1: #major
-            while len(key) < 7:
+    while len(key) < 7:
+        match key_type:
+            case 1: #major
                 if i == 3:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 2: #natural minor
-            while len(key) < 7:
-                if i == 2 and i == 5:
+            case 2: #natural minor
+                if i == 2 or i == 5:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 3: #harmonic minor
-            while len(key) < 7:
+            case 3: #harmonic minor
                 if i == 2 or i == 5:
                     key.append(key[i-1] + 1)
                 elif i == 6:
                     key.append(key[i-1] + 3)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 4: #dorian mode
-            while len(key) < 7:
+            case 4: #dorian mode
                 if i == 2 or i == 6:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 5: #mixolydian mode
-            while len(key) < 7:
+            case 5: #mixolydian mode
                 if i == 3 or i == 6:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 6: #lydian mode
-            while len(key) < 7:
+            case 6: #lydian mode
                 if i == 4:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 7: #phrygian mode
-            while len(key) < 7:
+            case 7: #phrygian mode
                 if i == 1 or i == 5:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
-        case 8: #locrian mode
-            while len(key) < 7:
+            case 8: #locrian mode
                 if i == 1 or i == 4:
                     key.append(key[i-1] + 1)
                 else:
                     key.append(key[i-1] + 2)
-                i += 1
-            return key
+
+        i += 1
+    return key
